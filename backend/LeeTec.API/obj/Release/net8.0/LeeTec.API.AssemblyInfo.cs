ceecb449fd0fc9b4d75a9593a288e63386d2f064
root <root@vmi3390887.contaboserver.net>
2026-07-13 12:35:46 +0200

    Remove unused count variable from enrol endpoint

--- backend/LeeTec.API/obj/Release/net8.0/LeeTec.API.AssemblyInfo.cs
+++ backend/LeeTec.API/obj/Release/net8.0/LeeTec.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeeTec.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db8f9854e87a8ee6766013c5c5617e31ff593674")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc08dff63ef9ddc899dadc5c7a2894e80bd46627")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeeTec.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeeTec.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
