409e34c24055366bd524b41f55fd18ff414d473f
root <root@vmi3390887.contaboserver.net>
2026-07-13 12:53:34 +0200

    Fix enrolment: use yearPrefix for LINQ compatibility

--- backend/LeeTec.API/obj/Release/net8.0/LeeTec.API.AssemblyInfo.cs
+++ backend/LeeTec.API/obj/Release/net8.0/LeeTec.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeeTec.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc08dff63ef9ddc899dadc5c7a2894e80bd46627")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ceecb449fd0fc9b4d75a9593a288e63386d2f064")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeeTec.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeeTec.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
