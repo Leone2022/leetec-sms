db8f9854e87a8ee6766013c5c5617e31ff593674
root <root@vmi3390887.contaboserver.net>
2026-07-13 12:10:27 +0200

    Fix student number generation to use MAX instead of COUNT

--- backend/LeeTec.API/obj/Release/net8.0/LeeTec.API.AssemblyInfo.cs
+++ backend/LeeTec.API/obj/Release/net8.0/LeeTec.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeeTec.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f908ee08f8c3d458ada19f1127e1272f3fca02b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f80776eb81c12ce2fcd3f7b35380a429dd2f442")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeeTec.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeeTec.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
